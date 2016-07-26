0b8dcbb1cfca705b1a462daf655cae90962f0fba
peternmcarthur <peter.n.mcarthur@googlemail.com>
2016-07-26 09:09:25 +0100

    added js test to play with

---
+++ test/index.js
@@ -0,0 +1,78 @@
+//	'use strict'
+//					const chai = require('chai')
+//const expect = chai.expect
+// ./README.md
+//describe("Vim practice", () => {
+//	
+//	it.skip("Delete the contents of a string", () => {
+//		
+//		let sentence = "I don't like vim"
+//		expect(sentence).to.equal("")
+//
+//	})
+//
+//	it.skip("The text will be copyed in to the variable", () => {
+//		
+//		let sentence = ""
+//		expect(sentence).to.equal("I like vim")
+//
+//	})
+//
+//	it.skip("The text will be replaced", () => {
+//		
+//		let sentence = "This text has nothing to do wit.skiph the expected resutl"
+//		expect(sentence).to.equal("I like vim")
+//
+//	})
+//
+//	it.skip("Delete a single world in a sentence", () => {
+//		
+//		let sentence = "I don't like vim"
+//		expect(sentence).to.equal("I like vim")
+//
+//	})
+//
+//	it.skip("Delete some it.skipems in an array", () => {
+//		
+//		let array = [
+//			"James",
+//			"Laura",
+//			"Paul",
+//			"Jane",
+//			"Sam",
+//			"Fred"
+//		]
+//		expect(array).to.eql(["James", "Fred"])
+//
+//	})
+//
+//	it.skip("Clear the contents of a block", () => {
+//		
+//		let sentence = "The function will be empty: function() { return \"this will be deleted\" }"
+//		expect(sentence).to.equal("The function will be empty: function() {}")
+//
+//	})
+//
+//	it.skip("A reason to use record mode", () => {
+//		
+//		let array = [
+//			"James",
+//			"Laura",
+//			"Paul",
+//			"Jane",
+//			"Sam",
+//			"Fred",
+//		]	
+//
+//		expect(array).to.eql([
+//			{ "name" : "James" },
+//			{ "name" : "Laura" },
+//			{ "name" : "Paul" },
+//			{ "name" : "Jane" },
+//			{ "name" : "Sam" },
+//			{ "name" : "Fred" }
+//		])
+//
+//	})
+//
+//})
